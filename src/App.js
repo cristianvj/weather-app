c3d7f230e47066cb3e45e9ac0449e3a7f7d9a747
cristianvj <cristiandavid.vj@gmail.com>
2019-12-29 09:23:50 -0500

    form problem fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,72 @@
-import React from 'react';
-import Header from './components/Header'
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Error from "./components/Error";
 
 function App() {
+
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    //prevenir ejecución
+    if(city === '') return;
+
+    const getAPI = async () => {
+      const appId = 'e17bb2aa01d4425d4b44bc1e61fd001f';
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${appId}`;
+      //consultar la URL
+      const reply = await fetch(url);
+      const result = await reply.json();
+  
+      console.log(result);
+    }
+     
+  }, [city, country]);
+
+  const dataSearch = data => {
+    //Validar que ambos campos existan
+    if(data.city === '' || data.country === ''){
+      //un error
+      setError(true);
+      return;      
+    }
+    //Ciudad y pais existen, agregarlos al state
+    setCity(data.city)
+    setCountry(data.country);
+    setError(false);
+  };
+
+  //Cargar componente condicionalmente
+  let component;
+  if (error) {
+    //Hay un error, mostrarlo
+    component = <Error message="Los dos campos son obligatorios" />;
+  }else{
+    //Mostrar el clima
+    component = null;
+  }
+
   return (
     <div className="App">
       <Header 
         title="Weather React App"
       />
+      <div className="container-form">
+        <div className="container">
+          <div className="row">
+            <div className="col s12 m6">
+              <Formulario 
+                dataSearch={ dataSearch }
+              />
+            </div>
+            <div className="col s12 m6">
+              { component }
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Formulario = () => {
+const Formulario = ({ dataSearch }) => {
+  
+  //State del componente
+  //busqueda = State, guardarBusqueda = setState({})
+  const [search, setSearch] = useState({
+    city: '',
+    country: '',
+  });
 
   const handleChange = e => {
     //Cambiar el state
-  }
+    setSearch({
+      ...search,
+      [e.target.name] : e.target.value,
+    });
+  };
+
+  const getWeather = e =>{
+    e.preventDefault();
+    //pasar directamente hacia el componente padre
+    dataSearch(search);
+  };
 
   return (
-    <form action="">
+    <form onSubmit={ getWeather } >
       <div className="input-field col s12">
         <input 
           type="text"
@@ -18,7 +35,7 @@ const Formulario = () => {
         <label htmlFor="city">City: </label>
       </div>
       <div className="input-field col s12">
-        <select onChange={hanldeChange} name="pais">
+        <select onChange={handleChange} name="country">
           <option value="">Selecciona un pais</option>
           <option value="US">The United States</option>
           <option value="AR">Argentina</option>
@@ -26,7 +43,7 @@ const Formulario = () => {
         </select>
       </div>
       <div className="input-field col s12">
-        <input type="text"/>
+        <input type="submit" className="waves-effect waves-light btn-large btn-block yellow accent-4" value="Buscar Clima" />
       </div>
     </form>
   );
